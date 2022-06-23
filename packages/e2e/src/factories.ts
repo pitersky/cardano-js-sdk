1e72b8df9c9c3fa4d55a0c2777983df2b5144a43
Angel Castillo <angel.castillob@protonmail.com>
2022-06-23 16:29:21 +0800

    refactor(e2e): migrated end-to-end tests from packages to the new e2e package

---
+++ packages/e2e/src/factories.ts
@@ -0,0 +1,233 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import {
+  Asset,
+  AssetProvider,
+  Cardano,
+  ChainHistoryProvider,
+  NetworkInfoProvider,
+  ProviderFactory,
+  RewardsProvider,
+  StakePoolProvider,
+  TxSubmitProvider,
+  UtxoProvider,
+  WalletProvider
+} from '@cardano-sdk/core';
+import {
+  BlockFrostAPI,
+  blockfrostAssetProvider,
+  blockfrostChainHistoryProvider,
+  blockfrostNetworkInfoProvider,
+  blockfrostRewardsProvider,
+  blockfrostTxSubmitProvider,
+  blockfrostUtxoProvider,
+  blockfrostWalletProvider
+} from '@cardano-sdk/blockfrost';
+import { CardanoWalletFaucetProvider, FaucetProvider } from './FaucetProvider';
+import { KeyManagement } from '@cardano-sdk/wallet';
+import { createStubStakePoolProvider } from '@cardano-sdk/util-dev';
+import DeviceConnection from '@cardano-foundation/ledgerjs-hw-app-cardano';
+
+const BLOCKFROST_PROVIDER = 'blockfrost';
+const BLOCKFROST_MISSING_PROJECT_ID = 'Missing project id';
+const KEY_AGENT_MISSING_MNEMONIC = 'Missing mnemonic words';
+const KEY_AGENT_MISSING_PASSWORD = 'Missing wallet password';
+const KEY_AGENT_MISSING_NETWORK_ID = 'Missing network id';
+const KEY_AGENT_MISSING_ACCOUNT_INDEX = 'Missing account index';
+
+// Sharing a single BlockFrostAPI object ensures rate limiting is shared across all blockfrost providers
+let blockfrostApi: BlockFrostAPI;
+
+/**
+ * Gets the singleton blockfrost API instance.
+ *
+ * @returns The blockfrost API instance, this function always returns the same instance.
+ */
+const getBlockfrostApi = async () => {
+  if (blockfrostApi !== undefined) return blockfrostApi;
+
+  if (process.env.BLOCKFROST_API_KEY === undefined) throw new Error(BLOCKFROST_MISSING_PROJECT_ID);
+
+  return new BlockFrostAPI({ isTestnet: true, projectId: process.env.BLOCKFROST_API_KEY });
+};
+
+export const faucetProviderFactory = new ProviderFactory<FaucetProvider>();
+export const keyManagementFactory = new ProviderFactory<KeyManagement.AsyncKeyAgent>();
+export const assetProviderFactory = new ProviderFactory<AssetProvider>();
+export const chainHistoryProviderFactory = new ProviderFactory<ChainHistoryProvider>();
+export const networkInfoProviderFactory = new ProviderFactory<NetworkInfoProvider>();
+export const rewardsProviderFactory = new ProviderFactory<RewardsProvider>();
+export const txSubmitProviderFactory = new ProviderFactory<TxSubmitProvider>();
+export const utxoProviderFactory = new ProviderFactory<UtxoProvider>();
+export const walletProviderFactory = new ProviderFactory<WalletProvider>();
+export const stakePoolProviderFactory = new ProviderFactory<StakePoolProvider>();
+
+// Faucet providers
+faucetProviderFactory.register('cardano-wallet', CardanoWalletFaucetProvider.create);
+
+// Asset providers
+
+/**
+ * Asset provider which does nothing.
+ */
+class NullAssetProvider implements AssetProvider {
+  getAsset() {
+    return new Promise<Asset.AssetInfo>((resolve) =>
+      resolve({
+        assetId: Cardano.AssetId(''),
+        fingerprint: Cardano.AssetFingerprint(''),
+        history: [
+          {
+            quantity: 0n,
+            transactionId: Cardano.TransactionId('')
+          }
+        ],
+        name: Cardano.AssetName(''),
+        policyId: Cardano.PolicyId(''),
+        quantity: 0n
+      } as Asset.AssetInfo)
+    );
+  }
+}
+
+assetProviderFactory.register(
+  'stub',
+  async (): Promise<AssetProvider> =>
+    new Promise<AssetProvider>(async (resolve) => {
+      resolve(new NullAssetProvider());
+    })
+);
+
+assetProviderFactory.register(
+  BLOCKFROST_PROVIDER,
+  async (): Promise<AssetProvider> =>
+    new Promise<AssetProvider>(async (resolve) => {
+      resolve(blockfrostAssetProvider(await getBlockfrostApi()));
+    })
+);
+
+// Chain history providers
+chainHistoryProviderFactory.register(
+  BLOCKFROST_PROVIDER,
+  async (): Promise<ChainHistoryProvider> =>
+    new Promise<ChainHistoryProvider>(async (resolve) => {
+      resolve(blockfrostChainHistoryProvider(await getBlockfrostApi()));
+    })
+);
+
+// Network info providers
+networkInfoProviderFactory.register(
+  BLOCKFROST_PROVIDER,
+  async (): Promise<NetworkInfoProvider> =>
+    new Promise<NetworkInfoProvider>(async (resolve) => {
+      resolve(blockfrostNetworkInfoProvider(await getBlockfrostApi()));
+    })
+);
+
+// Rewards providers
+rewardsProviderFactory.register(
+  BLOCKFROST_PROVIDER,
+  async (): Promise<RewardsProvider> =>
+    new Promise<RewardsProvider>(async (resolve) => {
+      resolve(blockfrostRewardsProvider(await getBlockfrostApi()));
+    })
+);
+
+// Tx submit providers
+txSubmitProviderFactory.register(
+  BLOCKFROST_PROVIDER,
+  async (): Promise<TxSubmitProvider> =>
+    new Promise<TxSubmitProvider>(async (resolve) => {
+      resolve(blockfrostTxSubmitProvider(await getBlockfrostApi()));
+    })
+);
+
+// Utxo providers
+utxoProviderFactory.register(
+  BLOCKFROST_PROVIDER,
+  async (): Promise<UtxoProvider> =>
+    new Promise<UtxoProvider>(async (resolve) => {
+      resolve(blockfrostUtxoProvider(await getBlockfrostApi()));
+    })
+);
+
+// Wallet providers
+walletProviderFactory.register(
+  blockfrostWalletProvider.name,
+  async (params: any): Promise<WalletProvider> =>
+    new Promise<WalletProvider>(async (resolve) => {
+      if (params.isTestnet === undefined) throw new Error(BLOCKFROST_MISSING_FLAG);
+
+      if (params.projectId === undefined) throw new Error(BLOCKFROST_MISSING_PROJECT_ID);
+
+      resolve(blockfrostWalletProvider(await getBlockfrostApi(params.isTestnet, params.projectId)));
+    })
+);
+
+// Stake Pool providers
+stakePoolProviderFactory.register(
+  'stub',
+  async (): Promise<StakePoolProvider> =>
+    new Promise<StakePoolProvider>(async (resolve) => {
+      resolve(createStubStakePoolProvider());
+    })
+);
+
+// Key Agents
+keyManagementFactory.register('inMemory', async (params: any): Promise<KeyManagement.AsyncKeyAgent> => {
+  const mnemonicWords = (params?.mnemonic || '').split(' ');
+
+  if (mnemonicWords.length === 0) throw new Error(KEY_AGENT_MISSING_MNEMONIC);
+
+  if (params.password === undefined) throw new Error(KEY_AGENT_MISSING_PASSWORD);
+
+  if (params.networkId === undefined) throw new Error(KEY_AGENT_MISSING_NETWORK_ID);
+
+  if (params.accountIndex === undefined) throw new Error(KEY_AGENT_MISSING_ACCOUNT_INDEX);
+
+  return KeyManagement.util.createAsyncKeyAgent(
+    await KeyManagement.InMemoryKeyAgent.fromBip39MnemonicWords({
+      accountIndex: params.accountIndex,
+      getPassword: async () => Buffer.from(params.password),
+      mnemonicWords,
+      networkId: params.networkId
+    })
+  );
+});
+
+keyManagementFactory.register('ledger', async (params: any): Promise<KeyManagement.AsyncKeyAgent> => {
+  if (params.networkId === undefined) throw new Error(KEY_AGENT_MISSING_NETWORK_ID);
+
+  if (params.accountIndex === undefined) throw new Error(KEY_AGENT_MISSING_ACCOUNT_INDEX);
+
+  let deviceConnection: DeviceConnection | null | undefined;
+  const ledgerKeyAgent = KeyManagement.LedgerKeyAgent.createWithDevice({
+    accountIndex: params.accountIndex,
+    communicationType: KeyManagement.CommunicationType.Node,
+    deviceConnection,
+    networkId: params.networkId,
+    protocolMagic: 1_097_911_063
+  });
+
+  return KeyManagement.util.createAsyncKeyAgent(await ledgerKeyAgent);
+});
+
+keyManagementFactory.register('trezor', async (params: any): Promise<KeyManagement.AsyncKeyAgent> => {
+  if (params.networkId === undefined) throw new Error(KEY_AGENT_MISSING_NETWORK_ID);
+
+  if (params.accountIndex === undefined) throw new Error(KEY_AGENT_MISSING_ACCOUNT_INDEX);
+
+  return KeyManagement.util.createAsyncKeyAgent(
+    await KeyManagement.TrezorKeyAgent.createWithDevice({
+      accountIndex: params.accountIndex,
+      networkId: params.networkId,
+      protocolMagic: 1_097_911_063,
+      trezorConfig: {
+        communicationType: KeyManagement.CommunicationType.Node,
+        manifest: {
+          appUrl: 'https://your.application.com',
+          email: 'email@developer.com'
+        }
+      }
+    })
+  );
+});
