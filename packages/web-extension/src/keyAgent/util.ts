f99963a9c85f120a8c0c8656b79237e3b1df1ffc
Tomislav Horaček <tomislav@tt-media.hr>
2022-06-23 09:50:25 +0200

    chore(web-extension): lint fix

--- packages/web-extension/src/keyAgent/util.ts
+++ packages/web-extension/src/keyAgent/util.ts
@@ -5,9 +5,9 @@ export const keyAgentChannel = (walletName: string) => `${walletName}$-keyAgent`
 
 export const keyAgentProperties: RemoteApiProperties<KeyManagement.AsyncKeyAgent> = {
   deriveAddress: RemoteApiPropertyType.MethodReturningPromise,
+  derivePublicKey: RemoteApiPropertyType.MethodReturningPromise,
   knownAddresses$: RemoteApiPropertyType.HotObservable,
   signBlob: RemoteApiPropertyType.MethodReturningPromise,
   signTransaction: RemoteApiPropertyType.MethodReturningPromise,
-  derivePublicKey: RemoteApiPropertyType.MethodReturningPromise,
   signVotingMetadata: RemoteApiPropertyType.MethodReturningPromise
 };
