593291217474ae298314ddd8dd7ade9268672747
Ivaylo Andonov <ivaylo.andonov@iohk.io>
2022-07-05 14:19:04 +0300

    fixup! feat(cardano-services): dns resolve services deps - add `--ogmios-srv-service-name` and `OGMIOS_SRV_SERVICE_NAME` - add `--rabbitmq-srv-service-name` and `RABBITMQ_SRV_SERVICE_NAME` - extend `TxSubmit` service with dns port resolution if srv name is used

--- packages/cardano-services/src/Program/errors/MissingProgramOption.ts
+++ packages/cardano-services/src/Program/errors/MissingProgramOption.ts
@@ -5,6 +5,8 @@ import { ServiceNames } from '../ServiceNames';
 export class MissingProgramOption extends CustomError {
   public constructor(service: ServiceNames, option: ProgramOptionDescriptions | ProgramOptionDescriptions[]) {
     super();
-    this.message = `${service} requires the ${typeof option === 'string' ? option : option.join('or')} program option.`;
+    this.message = `${service} requires the ${
+      typeof option === 'string' ? option : option.join(' or ')
+    } program option.`;
   }
 }
